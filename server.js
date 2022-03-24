2567e23fe2dee66526b4af92fe39d63d8e1b89f2
Rosa <ygao112@jhu.edu>
2022-03-24 19:24:02 -0400

    use express

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-const http = require("http");
-const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const app = express();
 
 const port = process.env.PORT || 4000;
 
-const file = fs.readFileSync("./index.html", "utf8");
-
-const server = http.createServer((request, response)=>{
-  response.writeHead(200, { "Content-Type": "text/html" });
-  response.write(file);
-  response.end();
+app.get("/", (req, res)=>{
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-server.listen(port);
-console.log("listening! (port ${port})");
+app.listen(port, ()=>{
+  console.log(`Express app listening at http://localhost:${port}`);
+});
